3b91b23fba1663d073bdc47089dd46c4606fd550
kliprift <eddie@worldone.com.my>
2016-08-23 19:44:12 +0800

    edit post controller with create and show action, added post show view file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,31 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
   end
+
+  def new
+  end
+
+  def create
+    @post=Post.new(post_params)
+    @post.save
+
+    redirect_to @post
+  end
+
+  def show
+  end
+
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+
+    def find_post
+      @post = Post.find(params[:id])
+    end
+
 end
