aefa9911e4b0293a50c5c3da8e147728aba5ba0f
kliprift <eddie@worldone.com.my>
2016-08-24 03:48:23 +0800

    added form partial, edit and delete post actions completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,20 @@ class PostsController < ApplicationController
   def edit
   end
 
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+
+    redirect_to root_path
+  end
+
 
   private
 
